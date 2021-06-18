8893613df94fa053ca107b8d4f9db02c226b9a5b
Isa Hashim <isah@earthlink.net>
2021-06-18 15:30:10 -0700

    Add Student Detail VC.

---
+++ StudentPortal/Student.swift
@@ -0,0 +1,15 @@
+//
+//  Student.swift
+//  StudentPortal
+//
+//  Created by Isa Hashim on 6/18/21.
+//
+
+import Foundation
+
+struct Student {
+    let firstName: String
+    let lastName: String
+    let email: String
+    let phone: String    
+}
---
+++ StudentPortal/StudentDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  StudentDetailViewController.swift
+//  StudentPortal
+//
+//  Created by Isa Hashim on 6/18/21.
+//
+
+import UIKit
+
+class StudentDetailViewController: UIViewController {
+    var student = Student(firstName: "Isa", lastName: "Hashim", email: "isa@foo.com", phone: "123-456-7890")
+    
+    @IBOutlet weak var studentNameLabel: UILabel!
+    @IBOutlet weak var studentEmailLabel: UILabel!
+    @IBOutlet weak var studentPhoneLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadStudent()
+    }
+    
+    func loadStudent() {
+        studentNameLabel.text = "\(student.firstName) \(student.lastName)"
+        studentEmailLabel.text = student.email
+        studentPhoneLabel.text = student.phone
+    }
+    
+    @IBAction func clickMeTapped(_ sender: Any) {
+        
+        print("Click me !!!")
+    }
+    
+
+}
